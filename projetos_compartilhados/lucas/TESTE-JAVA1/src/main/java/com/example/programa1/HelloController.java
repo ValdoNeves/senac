e3591d2fa6f087be670106a2aa6b4e578656d77c
Lucas_Araujo6 <lucaspedro142009@hotmail.com>
2022-03-08 23:56:57 -0300

    Atualização repositorio

---
+++ projetos_compartilhados/lucas/TESTE-JAVA1/src/main/java/com/example/programa1/HelloController.java
@@ -0,0 +1,30 @@
+package com.example.programa1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    protected void onHelloButtonClick() {
+
+        String Entrei = input.getText();
+        if(Entrei == "") {
+            welcomeText.setText("Digite um valor no campo Acima");
+        }
+        else {
+            welcomeText.setText(Entrei + " adora pão de queijo");
+            input.setText("");
+        }
+
+    }
+
+    @FXML
+    protected void off() { welcomeText.setText("Acabou"); }
+}
