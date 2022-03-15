7926c0878c4d8364bc5ef072c6839e0d719b55a7
Lucas_Araujo6 <lucaspedro142009@hotmail.com>
2022-03-14 23:44:01 -0300

    Novo teste

---
+++ projetos_compartilhados/lucas/TESTE-JAVA4/src/main/java/com/example/testejava4/Main.java
@@ -0,0 +1,13 @@
+package com.example.testejava4;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Menu hbacon =  new Menu("Hambuguer Bacon", "O unico da região", 23.99);
+       hbacon.menu2();
+
+    }
+
+
+}
