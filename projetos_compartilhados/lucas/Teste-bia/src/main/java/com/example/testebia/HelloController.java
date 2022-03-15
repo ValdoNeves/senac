296fb42ca46e2804ccdcf76e3b6f7458ab2b0494
ValdoNeves <valdogt1@gmail.com>
2022-03-14 22:46:03 -0300

    inseriendo logica inicial calculadora

--- projetos_compartilhados/lucas/Teste-bia/src/main/java/com/example/testebia/HelloController.java
+++ projetos_compartilhados/lucas/Teste-bia/src/main/java/com/example/testebia/HelloController.java
@@ -1,13 +1,12 @@
 package com.example.testebia;
 
+import com.example.actions.Calcular;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
-
-import static java.lang.Double.parseDouble;
 
 public class HelloController {
 
@@ -45,7 +44,10 @@ public class HelloController {
        String n2 = nota2.getText();
        String n3 = nota3.getText();
 
-       double soma = (parseDouble(n1) + parseDouble(n2) + parseDouble(n3)) /3;
+
+
+//       double soma = (parseDouble(n1) + parseDouble(n2) + parseDouble(n3)) /3;
+       double soma = new Calcular().Media(n1, n2, n3);
 
        if(soma >= 7){
 
@@ -56,6 +58,8 @@ public class HelloController {
            resultadotext.setText("Reprovado, Parabéns! ");
            tela.setStyle("-fx-background-color: linear-gradient(to bottom,#ff3d3d, #a00000)");
        }
-        resultado.setText("" + soma);
+
+        resultado.setText(Double.toString(soma));
+//        System.out.printf("%.1f", soma);
     }
 }
